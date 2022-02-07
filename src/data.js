1d0b1cc9d8bdb5929a60b79da4bafb6a5c9228b1
Victor M <7806034@mail.ru>
2022-02-07 21:49:34 +0200

    init_3

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const products = [{
+        id: 1,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/vJsGPZ3/Cat-004.png",
+        link: "http://lama.dev",
+    },
+];
